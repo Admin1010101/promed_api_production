e51d94e30d7399cc7e93bc393494d23bf8fa06c3
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-15 05:48:21 -0500

    fix: configuring jazzmin

--- promed_backend_api/settings.py
+++ promed_backend_api/settings.py
@@ -214,16 +214,16 @@ logger.info("=" * 80)
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Library Admin",
+    "site_title": "Promed Health Plus",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "",
+    "site_header": "Promed Health Plus",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Promed Health Plus",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "static/imagees/logo.png",
+    "site_logo": "/imagees/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
@@ -234,7 +234,6 @@ JAZZMIN_SETTINGS = {
     # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
 
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": None,
 
     # Welcome text on the login screen
@@ -243,48 +242,16 @@ JAZZMIN_SETTINGS = {
     # Copyright on the footer
     "copyright": "Promed Health Plus",
 
-    # List of model admins to search from the search bar, search bar omitted if excluded
-    # If you want to use a single search field you dont need to use a list, you can use a simple string 
     "search_model": ["auth.User", "auth.Group"],
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
 
-    ############
-    # Top Menu #
-    ############
-
-    # Links to put along the top menu
-    # "topmenu_links": [
-
-    #     # Url that gets reversed (Permissions can be added)
-    #     {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
-
-    #     # external url that opens in a new window (Permissions can be added)
-    #     {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
-
-    #     # model admin to link to (Permissions checked against model)
-    #     {"model": "auth.User"},
-
-    #     # App with dropdown menu to all its models pages (Permissions checked against models)
-    #     {"app": "books"},
-    # ],
-
-    #############
-    # User Menu #
-    #############
-
-    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
     "usermenu_links": [
         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
         {"model": "auth.user"}
     ],
 
-    #############
-    # Side Menu #
-    #############
-
-    # Whether to display the side menu
     "show_sidebar": True,
 
     # Whether to aut expand the menu
@@ -309,8 +276,6 @@ JAZZMIN_SETTINGS = {
         }]
     },
 
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
@@ -318,18 +283,7 @@ JAZZMIN_SETTINGS = {
     },
     # Icons that are used when one is not manually specified
     "default_icon_parents": "fas fa-chevron-circle-right",
-    "default_icon_children": "fas fa-circle",
-
-    #################
-    # Related Modal #
-    #################
-    # Use modals instead of popups
     "related_modal_active": False,
-
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
     "custom_css": None,
     "custom_js": None,
     # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
@@ -337,20 +291,28 @@ JAZZMIN_SETTINGS = {
     # Whether to show the UI customizer on the sidebar
     "show_ui_builder": False,
 
-    ###############
-    # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
-    "changeform_format": "horizontal_tabs",
-    # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
-    # Add a language dropdown into the admin
-    "language_chooser": True,
+}
+
+# settings.py
+
+JAZZMIN_UI_TWEAKS = {
+    # Set the default theme for light mode
+    "theme": "flatly", 
+
+    # Set a separate theme to use when the user's device is in dark mode
+    # If this is omitted, only the 'theme' above will be used.
+    "dark_mode_theme": "darkly", 
+
+    # Other tweaks (optional)
+    "navbar_small_text": False,
+    "footer_small_text": True,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-success", # The color of the brand/logo area
+    "accent": "accent-teal",         # The color used for links/buttons
+    "navbar": "navbar-dark",         # The style of the top navigation bar
+    "navbar_fixed": True,
+    "footer_fixed": False,
 }
 
 
