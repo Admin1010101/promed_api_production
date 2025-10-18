18b722a181fdad23bc5dab8322231335896a5696
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-17 20:05:56 -0500

    fixed: patient serializer

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,9 +1,14 @@
+# patients/serializers.py
 from rest_framework import serializers
 from .models import Patient
+
 class PatientSerializer(serializers.ModelSerializer):
-    provider = serializers.PrimaryKeyRelatedField(read_only=True)  # Can be replaced with a nested serializer
+    provider = serializers.PrimaryKeyRelatedField(read_only=True)
+    full_name = serializers.CharField(read_only=True)  # Add this for the property
+    
     class Meta:
         model = Patient
         fields = '__all__'
+        read_only_fields = ['provider', 'date_created', 'date_updated']
         
 
