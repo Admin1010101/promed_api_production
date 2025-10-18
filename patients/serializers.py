5d8d6c3ca4685a28ce00e5ece1cab25c7457a7c8
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-17 20:52:28 -0500

    fixed: patient serializer

--- patients/serializers.py
+++ patients/serializers.py
@@ -4,11 +4,23 @@ from .models import Patient
 
 class PatientSerializer(serializers.ModelSerializer):
     provider = serializers.PrimaryKeyRelatedField(read_only=True)
-    full_name = serializers.CharField(read_only=True)  # Add this for the property
+    full_name = serializers.CharField(read_only=True)
     
     class Meta:
         model = Patient
         fields = '__all__'
-        read_only_fields = ['provider', 'date_created', 'date_updated']
+        read_only_fields = ['id', 'provider', 'date_created', 'date_updated', 'full_name']
         
-
+    def validate(self, data):
+        """Extra safety: Remove provider fields if they somehow get through"""
+        data.pop('provider', None)
+        data.pop('provider_id', None)
+        return data
+    
+    def to_internal_value(self, data):
+        """Remove provider fields before validation"""
+        if isinstance(data, dict):
+            data = data.copy()
+            data.pop('provider', None)
+            data.pop('provider_id', None)
+        return super().to_internal_value(data)
