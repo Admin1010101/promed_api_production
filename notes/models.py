67de2ee7c1fab273b71a29ca7d5062f1e201f801
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-19 07:17:46 -0500

    fixed: notes backend api

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,4 @@
+# notes/models.py
 from django.db import models
 from patients.models import Patient
 
@@ -6,11 +7,13 @@ class Notes(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     document = models.FileField(upload_to='notes_docs/', null=True, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)  # Fixed typo
-    date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)      # Fixed typo
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f'{self.patient} | {self.title}'
     
     class Meta:
         ordering = ['-date_created']  # Most recent first
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes'
