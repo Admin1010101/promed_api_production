78de8407cb3f09e13aec7419492163b97de61ead
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-19 06:49:43 -0500

    fixed: notes backend api

--- notes/models.py
+++ notes/models.py
@@ -6,8 +6,8 @@ class Notes(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     document = models.FileField(upload_to='notes_docs/', null=True, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)  # Fixed typo
-    date_updated = models.DateTimeField(auto_now=True)      # Fixed typo
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)  # Fixed typo
+    date_updated = models.DateTimeField(auto_now=True, null=True, blank=True)      # Fixed typo
     
     def __str__(self):
         return f'{self.patient} | {self.title}'
