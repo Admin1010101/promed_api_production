f91d5d4a0ebf4249da6826ef319212ad06da702f
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-20 00:12:18 -0500

    fixed: pdf email

--- onboarding_ops/views.py
+++ onboarding_ops/views.py
@@ -213,7 +213,7 @@ def save_new_account_form(request):
         }
         
         # Render HTML template
-        html_string = render_to_string('onboarding_ops/new_account_form_pdf.html', context)
+        html_string = render_to_string('onboarding_ops/new_account_form_submission.html', context)
         
         # Generate PDF using xhtml2pdf
         pdf_buffer = BytesIO()
