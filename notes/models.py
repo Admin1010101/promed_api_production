580e7e8801a77dd132bb32f57507294fb7f3fe42
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-20 23:17:57 -0500

    fixed patient urls

--- notes/models.py
+++ notes/models.py
@@ -8,7 +8,7 @@ class Notes(models.Model):
     body = models.TextField()
     document = models.FileField(upload_to='notes_docs/', null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    date_updated = models.DateTimeField(auto_now=True, blank=True, name=True)
+    date_updated = models.DateTimeField(auto_now=True, blank=True, null=True)
     
     def __str__(self):
         return f'{self.patient} | {self.title}'
