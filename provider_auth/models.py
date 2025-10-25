8b03a28647e6a93c7edf563a9b5ef437fb66c701
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-25 17:28:41 -0500

    fixed: import error

--- provider_auth/models.py
+++ provider_auth/models.py
@@ -63,7 +63,6 @@ class User(AbstractUser):
     is_approved = models.BooleanField(default=False)
     welcome_email_sent = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
-    baa_signed = models.BooleanField(null=True)
     has_signed_baa = models.BooleanField(default=False)
     baa_signed_at = models.DateTimeField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
